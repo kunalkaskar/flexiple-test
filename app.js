c3915f6a3e7b30867fb2e02f7b2dc94dd46828ba
Kunal <kunal@adolpha.com>
2017-06-03 23:19:41 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,42 @@
+angular.module('flexiple', [])
+.controller('postProjectController', ['$scope', function($scope) {
+
+	$scope.formData = {
+		resourceType : "",
+		duration	: ""
+	}
+	$scope.isContentWriter = false;
+	$scope.isDeveloper = false;
+	$scope.formValid = false;
+
+	$scope.$watch('formData.resourceType', function(value) {
+			console.log(value);
+	       if(value == "dev"){
+	       		$scope.isDeveloper = true;
+	       		$scope.isContentWriter = false;
+	       }
+	       if(value == "cont_write") {
+	       		$scope.isDeveloper = false;
+	       		$scope.isContentWriter = true;
+	       }
+	});
+
+	$scope.$watch('formData.contentType', function(value) {
+			console.log(value);
+	       if(value == "blog"){
+	       		$scope.isBlog = true;
+	       		$scope.isCopyWrite = false;
+	       }
+	       if(value == "copywrite") {
+	       		$scope.isBlog = false;
+	       		$scope.isCopyWrite = true;
+	       }
+	});
+
+	$scope.submitted = false;
+	$scope.postProject = function(project) {
+		if(project.$valid){
+			$scope.formValid = true;
+		}
+	}
+}]);
